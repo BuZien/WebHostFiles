84807130c796a99192377776afed6e227493bb33
BuZien <75002846+BuZien@users.noreply.github.com>
2021-04-01 20:15:27 +0200

    Update zCon.js

--- zTheme/zCon.js
+++ zTheme/zCon.js
@@ -1,6 +1,9 @@
+setTimeout(()=>console.clear(),500);
+setTimeout(()=>console.clear(),1000);
+setTimeout(()=>console.clear(),1500);
 setInterval(()=>""
 +console.clear()+console.log('%c Zien Template - V 1.1', 'color:#3838da;font-size:11px;font-weight:900;font-family:tahoma;display:block')
 +console.log('%c Credits:', 'background:#ddd;padding:2px 5px;color:#3838da;font-size:10px;font-weight:600', 'DoubleA')
 +console.log('%c URL:', 'background:#ddd;padding:2px 5px;color:#3838da;font-size:10px;font-weight:600', 'https://zien-template.blogspot.com')
 +console.log('%c Version:', 'background:#ddd;padding:2px 5px;color:#3838da;font-size:10px;font-weight:600', '1.1.0 Premium')
-+"",1500);
++"",2000);
