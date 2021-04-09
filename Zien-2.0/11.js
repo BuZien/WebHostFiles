fa53c39579322d7ff4f3e6877dd58b64f52f50c0
BuZien <75002846+BuZien@users.noreply.github.com>
2021-04-09 19:26:32 +0200

    Update 11.js

--- Zien-2.0/11.js
+++ Zien-2.0/11.js
@@ -179,38 +179,4 @@ window.addEventListener("scroll", function () {
         n.next().hasClass("show") ? (n.next().removeClass("show"), n.next().slideUp(350)) : (n.parent().parent().find("li .inner").removeClass("show"), n.parent().parent().find("li .inner").slideUp(350), n.next().toggleClass("show"), n.next().slideToggle(350))
     })
 });
-var homePage = window.location.protocol+"//"+window.location.hostname,
-    maxResults = 5,
-    containerId = 'zRandomPosts';
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function shuffleArray(arr) {
-    var i = arr.length, j, temp;
-    if (i === 0) return false;
-    while (--i) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    return arr;
-}
-function zRandom(json) {
-    var startIndex = getRandomInt(1, (json.feed.openSearch$totalResults.$t - maxResults));
-    document.write('<scr' + 'ipt src="' + homePage + '/feeds/posts/summary?alt=json-in-script&orderby=updated&start-index=' + startIndex + '&max-results=' + maxResults + '&callback=randomPosts"></scr' + 'ipt>');
-}
-function randomPosts(json) {
-    var link, ct = document.getElementById(containerId),
-        entry = shuffleArray(json.feed.entry),
-        skeleton = "<ul>";
-    for (var i = 0, len = entry.length; i < len; i++) {
-        for (var j = 0, jen = entry[i].link.length; j < jen; j++) {
-            link = (entry[i].link[j].rel == "alternate") ? entry[i].link[j].href : '#';
-        }
-        skeleton += '<li><a href="' + link + '">' + entry[i].title.$t + '</a></li>';
-    }
-    ct.innerHTML = skeleton + '</ul>';
-}
-document.write('<scr' + 'ipt src="' + homePage + '/feeds/posts/summary?alt=json-in-script&max-results=0&callback=zRandom"></scr' + 'ipt>');
 setInterval(() => console.clear(), 100);
